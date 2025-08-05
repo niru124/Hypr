42dda5a1c5aa5408150177d0446c8fede525731c
niru124 <nirantarkp124@gmail.com>
2025-08-05 13:33:08 +0530

    nvim config

---
+++ Config/.config/nvim_1/lua/plugins/astrocore.lua
@@ -0,0 +1,164 @@
+-- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
+-- Configuration documentation can be found with `:h astrocore`
+-- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
+--       as this provides autocomplete and documentation while editing
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    -- Configure core features of AstroNvim
+    features = {
+      large_buf = {
+        size = 136 * 32,
+        lines = 10000,
+      }, -- set global limits for large files for disabling features like treesitter
+      autopairs = true, -- enable autopairs at start
+      cmp = true, -- enable completion at start
+      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+      highlighturl = true, -- highlight URLs at start
+      notifications = true, -- enable notifications at start
+    },
+    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+    diagnostics = {
+      virtual_text = true,
+      underline = true,
+    },
+    -- vim options can be configured here
+    options = {
+      opt = { -- vim.opt.<key>
+        relativenumber = true, -- sets vim.opt.relativenumber
+        number = true, -- sets vim.opt.number
+        spell = false, -- sets vim.opt.spell
+        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+        wrap = true, -- sets vim.opt.wrap
+      },
+      g = { -- vim.g.<key>
+        -- configure global vim variables (vim.g)
+        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+        -- This can be found in the `lua/lazy_setup.lua` file
+      },
+    },
+    -- Mappings can be configured through AstroCore as well.
+    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+    mappings = {
+      -- first key is the mode
+      n = {
+        -- second key is the lefthand side of the map
+
+        -- navigate buffer tabs
+        -- Change leader + Shift+h to go to the previous buffer
+        vim.api.nvim_set_keymap(
+          "n",
+          "<S-h>",
+          '<Cmd>lua require("astrocore.buffer").nav(-1)<CR>',
+          {
+            noremap = true,
+            silent = true,
+            desc = "Previous buffer",
+          }
+        ),
+
+        -- Change leader + Shift+l to go to the next buffer
+        vim.api.nvim_set_keymap(
+          "n",
+          "<S-l>",
+          '<Cmd>lua require("astrocore.buffer").nav(1)<CR>',
+          {
+            noremap = true,
+            silent = true,
+            desc = "Next buffer",
+          }
+        ),
+
+        -- mappings seen under group name "Buffer"
+        ["<Leader>bd"] = {
+          function()
+            require("astroui.status.heirline").buffer_picker(
+              function(bufnr)
+                require("astrocore.buffer").close(
+                  bufnr
+                )
+              end
+            )
+          end,
+          desc = "Close buffer from tabline",
+        },
+        vim.api.nvim_set_keymap(
+          "n",
+          "<Leader><Leader>",
+          "<Cmd>Telescope find_files<CR>",
+          {
+            noremap = true,
+            silent = true,
+            desc = "Telescope Find Files",
+          }
+        ),
+        vim.api.nvim_set_keymap(
+          "n",
+          "<Leader><Leader>b",
+          "<Cmd>Telescope buffers<CR>",
+          {
+            noremap = true,
+            silent = true,
+            desc = "Telescope Buffers",
+          }
+        ),
+
+        -- Custom function to toggle floating terminal
+        -- Set up toggleterm with floating window option
+        require("toggleterm").setup {
+          size = 20, -- You can customize the size of the floating terminal if needed
+          direction = "float", -- Set terminal direction to 'float'
+          float_opts = {
+            border = "rounded", -- Set border style (e.g., 'rounded', 'single', 'double', etc.)
+          },
+          open_mapping = [[<c-\>]], -- Optional: Set the default toggle mapping (you can remove this line)
+        },
+
+        vim.api.nvim_set_keymap('n', '<S-e>', '$', { noremap = true, silent = true }),
+        vim.api.nvim_set_keymap('n', '<S-b>', '0', { noremap = true, silent = true }),
+        vim.api.nvim_set_keymap(
+          "t",
+          "<C-n>",
+          [[<C-\><C-n>]],
+          {
+            noremap = true,
+            silent = true,
+            desc = "Go to Normal Mode in Terminal",
+          }
+        ),
+        -- Map Ctrl+/ to toggle the terminal as a floating window
+        vim.api.nvim_set_keymap(
+          "n",
+          "<C-/>",
+          "<Cmd>ToggleTerm direction=float<CR>",
+          {
+            noremap = true,
+            silent = true,
+            desc = "Toggle Floating Terminal",
+          }
+        ),
+        -- tables with just a `desc` key will be registered with which-key if it's installed
+        -- this is useful for naming menus
+        -- ["<Leader>b"] = { desc = "Buffers" },
+
+        -- setting a mapping to false will disable it
+        -- ["<C-S>"] = false,
+        vim.api.nvim_set_keymap(
+          "n",
+          "<Leader>th",
+          ":ToggleTerm direction=horizontal<CR>",
+          { noremap = true, silent = true }
+        ),
+        vim.api.nvim_set_keymap(
+          "n",
+          "<Leader>tv",
+          ":ToggleTerm direction=vertical<CR>",
+          { noremap = true, silent = true }
+        ),
+      },
+    },
+  },
+}
---
+++ Config/.config/nvim_1/lua/plugins/nvim-aider.lua
@@ -0,0 +1,70 @@
+return {
+  {
+    "GeorgesAlkhouri/nvim-aider",
+    cmd = "Aider",
+    keys = {
+      {
+        "<leader>a/",
+        "<cmd>Aider toggle<cr>",
+        desc = "Toggle Aider",
+      },
+      {
+        "<leader>as",
+        "<cmd>Aider send<cr>",
+        desc = "Send to Aider",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>ac",
+        "<cmd>Aider command<cr>",
+        desc = "Aider Commands",
+      },
+      {
+        "<leader>ab",
+        "<cmd>Aider buffer<cr>",
+        desc = "Send Buffer",
+      },
+      {
+        "<leader>a+",
+        "<cmd>Aider add<cr>",
+        desc = "Add File",
+      },
+      {
+        "<leader>a-",
+        "<cmd>Aider drop<cr>",
+        desc = "Drop File",
+      },
+      {
+        "<leader>ar",
+        "<cmd>Aider add readonly<cr>",
+        desc = "Add Read-Only",
+      },
+      {
+        "<leader>a+",
+        "<cmd>AiderTreeAddFile<cr>",
+        desc = "Add File from Tree to Aider",
+        ft = "NvimTree",
+      },
+      {
+        "<leader>a-",
+        "<cmd>AiderTreeDropFile<cr>",
+        desc = "Drop File from Tree from Aider",
+        ft = "NvimTree",
+      },
+    },
+    dependencies = {
+      "folke/snacks.nvim",
+      "catppuccin/nvim",
+      "nvim-tree/nvim-tree.lua",
+      {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = function(_, opts)
+          require("nvim_aider.neo_tree").setup(
+            opts
+          )
+        end,
+      },
+    },
+    config = true,
+  },
+}
