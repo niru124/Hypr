eb34671774218fba2bae104aff821a5cdd816ca6
niru124 <nirantarkp124@gmail.com>
2026-01-18 15:02:02 +0530

    update install_wallapaper and better oil.nvim conf with custom floating support

--- Config/.config/nvim/lua/lazy_setup.lua
+++ Config/.config/nvim/lua/lazy_setup.lua
@@ -14,31 +14,6 @@ require("lazy").setup({
   { import = "community" },
   { import = "plugins" },
   { "David-Kunz/gen.nvim" },
-  {
-    "mikavilpas/yazi.nvim",
-    event = "VeryLazy",
-    keys = {
-      -- 👇 in this section, choose your own keymappings!
-      {
-        "<leader>e",
-        "<cmd>Yazi<cr>",
-        desc = "Open yazi at the current file",
-      },
-      {
-        -- Open in the current working directory
-        "<leader>cw",
-        "<cmd>Yazi cwd<cr>",
-        desc = "Open the file manager in nvim's working directory",
-      },
-      {
-        -- NOTE: this requires a version of yazi that includes
-        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-        "<c-up>",
-        "<cmd>Yazi toggle<cr>",
-        desc = "Resume the last yazi session",
-      },
-    },
-  },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "catppuccin" } },
